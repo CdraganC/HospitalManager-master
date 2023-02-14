f78156d0e762c4aa0278ac4c7f9eb832bba1c6c7
andreiveres <andrei.veres@fanduel.com>
2023-02-14 20:48:19 +0200

    local date time format for medication list

--- src/main/java/com/siit/hospital_manager/model/MedicationHistory.java
+++ src/main/java/com/siit/hospital_manager/model/MedicationHistory.java
@@ -3,12 +3,13 @@ package com.siit.hospital_manager.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
-import java.util.Date;
+import java.lang.constant.Constable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
-@Table(name="medication")
+@Table(name = "medication")
 @Data
 @RequiredArgsConstructor
 public class MedicationHistory {
@@ -17,18 +18,22 @@ public class MedicationHistory {
     private Integer id;
     private String medication;
     private String drugName;
-    private Date medicationFrom;
-    private Date medicationTo;
+    private LocalDateTime medicationFrom;
+    private LocalDateTime medicationTo;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "patient_id", nullable = false)
     private Patient patient;
 
+    public Constable formattedDateFromMethod() {
+        String dateFormat = "yyyy-MM-dd";
+        String formattedDateFrom = medicationFrom.format(DateTimeFormatter.ofPattern(dateFormat));
+        return formattedDateFrom;
+    }
 
-
-
-
-
-
-
+    public Constable formattedDateToMethod() {
+        String dateFormat = "yyyy-MM-dd";
+        String formattedDateTo = medicationTo.format(DateTimeFormatter.ofPattern(dateFormat));
+        return formattedDateTo;
+    }
 }
--- src/main/java/com/siit/hospital_manager/model/dto/MedicationHistoryDto.java
+++ src/main/java/com/siit/hospital_manager/model/dto/MedicationHistoryDto.java
@@ -1,11 +1,10 @@
 package com.siit.hospital_manager.model.dto;
 
 import com.siit.hospital_manager.model.MedicationHistory;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import java.util.Date;
+import java.lang.constant.Constable;
+
 @Getter
 @Setter
 @RequiredArgsConstructor
@@ -14,14 +13,14 @@ public class MedicationHistoryDto {
     private Integer id;
     private String medication;
     private String drugName;
-    private Date medicationFrom;
-    private Date medicationTo;
+    private Constable medicationFrom;
+    private Constable medicationTo;
 
-    public MedicationHistoryDto (MedicationHistory medicationHistory) {
+    public MedicationHistoryDto(MedicationHistory medicationHistory) {
         this.id = medicationHistory.getId();
         this.medication = medicationHistory.getMedication();
         this.drugName = medicationHistory.getDrugName();
-        this.medicationFrom = medicationHistory.getMedicationFrom();
-        this.medicationTo = medicationHistory.getMedicationTo();
+        this.medicationFrom = medicationHistory.formattedDateFromMethod();
+        this.medicationTo = medicationHistory.formattedDateToMethod();
     }
 }
